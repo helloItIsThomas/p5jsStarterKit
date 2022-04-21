388abaf7e0d004d5a8740d6caac1bf02d39aa7a1
Thomas McElmeel <81656327+helloItIsThomas@users.noreply.github.com>
2022-04-21 14:54:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  libSetup();
+  messageOne = "welcome :)";
+  messageTwo = "~(°_°~)";
+}
+
+function draw(){
+  background(color1);
+  // write your functions / sketch here
+  typeDrawStart(width/2,height/2,100);
+}
